7426ba32180d74c93b22fa72ece2d11376e384a7
root <root@localhost.localdomain>
2018-11-02 22:09:09 -0400

    update new files

--- build.tf
+++ build.tf
@@ -1,12 +1,17 @@
 provider "aws" {
-  access_key = "AKIAI5TTBSBTNPMNREMA"
-  secret_key = "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"
+  access_key = "AKIAJGQMEI45D3MYLFDQ"
+  secret_key = "HsQ15AXoX6jz1rWiJlekhvrsJySnOHRkdqUrPIcv"
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-6871a115"
-  instance_type = "t2.micro"
-  key_name = "deploy"
+
+variable "instance_type" { default="t2.micro"}
+variable "ami" { default="ami-6871a115" }
+
+resource "aws_instance" "ec2" {
+  count         = 3 
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name      = "deploy"
 }
 
